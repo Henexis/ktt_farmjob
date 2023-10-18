66d1db99f42271b78865f6f240f18c791c9a5839
T5.Prime <106626643+Henexis@users.noreply.github.com>
2023-10-18 15:17:43 +0200

    Update config.lua

--- ktt_farmjob/shared/config.lua
+++ ktt_farmjob/shared/config.lua
@@ -5,10 +5,10 @@ KloudDev.Debug = false
 KloudDev.ImagePath = "https://cfx-nui-ox_inventory/web/images/"
 
 KloudDev.DrawSprite = true -- ox_target indicator
-KloudDev.DrawText = "ox" -- "qb","ox"
-KloudDev.Menu = "ox" -- "qb","ox"
-KloudDev.Notify = "ox" -- "qb","esx","ox", "ps"
-KloudDev.NotifyPos = "top-right" -- For ox_lib // 'top', 'top-right', 'top-left', 'bottom', 'bottom-right', 'bottom-left', 'center-right', 'center-left' // For QB change qb-core/config.lua
+KloudDev.DrawText = "qb" -- "qb","ox"
+KloudDev.Menu = "qb" -- "qb","ox"
+KloudDev.Notify = "qb" -- "qb","esx","ox", "ps"
+KloudDev.NotifyPos = "bottom" -- For ox_lib // 'top', 'top-right', 'top-left', 'bottom', 'bottom-right', 'bottom-left', 'center-right', 'center-left' // For QB change qb-core/config.lua
 KloudDev.Progress = "ox-circle" -- "ox-bar","ox-circle"
 KloudDev.ProgressCirclePos = "bottom" -- "middle", "bottom"
-KloudDev.DrawTextAlignment = "right" -- "top","right","left"
+KloudDev.DrawTextAlignment = "left" -- "top","right","left"
